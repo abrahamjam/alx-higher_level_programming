b0f840aa720f230c4d1e26b304ee6ec70342e00f
abrahamjam <abrahamjms156@gmail.com>
2024-03-28 13:54:51 -0700

    What's my status? #0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 import urllib.request
 
+"""
+Python script that fetches https://alx-intranet.hbtn.io/status
+using the package urllib
+Not allowed to import any packages other than urllib
+The body of the response must be displayed like the following example (tabulation before -)
+"""
 if __name__ == "__main__":
     url = 'https://alx-intranet.hbtn.io/status'
     with urllib.request.urlopen(url) as response:
