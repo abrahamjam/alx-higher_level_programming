a1bd8300c8fca770ce725ba9aaab84b0fd41bf80
abrahamjam <abrahamjms156@gmail.com>
2024-03-28 14:15:47 -0700

    Search API

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -2,6 +2,15 @@
 import requests
 import sys
 
+"""
+Python script that takes in a letter and sends a POST request to 
+http://0.0.0.0:5000/search_user with the letter as a parameter.
+The letter must be sent in the variable q
+If no argument is given, set q=""
+If the response body is properly JSON formatted and not empty, 
+display the id and name like this: [<id>] <name>
+"""
+
 if __name__ == "__main__":
     q = sys.argv[1] if len(sys.argv) > 1 else ""
     payload = {'q': q}
