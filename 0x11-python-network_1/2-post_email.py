eb24be4d0a2ce83eccc98cb6deeecbd44a34312c
abrahamjam <abrahamjms156@gmail.com>
2024-03-28 14:02:49 -0700

    POST an email #0

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -3,6 +3,12 @@ import urllib.request
 import urllib.parse
 import sys
 
+"""
+Python script that takes in a URL and an email, sends a POST request to the passed 
+URL with the email as a parameter, and displays the body of the response (decoded in utf-8)
+The email must be sent in the email variable
+"""
+
 if __name__ == "__main__":
     if len(sys.argv) < 3:
         print("Usage: {} <URL> <email>".format(sys.argv[0]))
