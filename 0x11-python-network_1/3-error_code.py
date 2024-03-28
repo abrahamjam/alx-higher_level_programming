28b6ff5b9a55e8eb811a94a23aaff0579e7e03f9
abrahamjam <abrahamjms156@gmail.com>
2024-03-28 14:06:52 -0700

    Error code #0

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -3,6 +3,14 @@ import urllib.request
 import urllib.error
 import sys
 
+"""
+Python script that takes in a URL, sends a request to the URL 
+and displays the body of the response (decoded in utf-8).
+manage urllib.error.HTTPError exceptions and print: Error code: 
+followed by the HTTP status code
+use the packages urllib and sys
+"""
+
 if __name__ == "__main__":
     url = sys.argv[1]
     try:
