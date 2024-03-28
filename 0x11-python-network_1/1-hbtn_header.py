3e86b934dab1d0c202c6bf1b8a1ae4a376436dd4
abrahamjam <abrahamjms156@gmail.com>
2024-03-28 13:59:24 -0700

    Response header value #0

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,6 +2,15 @@
 import urllib.request
 import sys
 
+"""
+Python script that takes in a URL, sends a request to the URL and 
+displays the value of the X-Request-Id variable found in the header of the response.
+using the packages urllib and sys
+not allow to import packages other than urllib and sys
+The value of this variable is different for each request
+No need to check arguments passed to the script (number or
+"""
+
 if __name__ == "__main__":
     url = sys.argv[1]
     with urllib.request.urlopen(url) as response:
